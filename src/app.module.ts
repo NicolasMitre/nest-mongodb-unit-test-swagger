028dac026abbfa2bf4e6231446d9523a91b490a3
NicolasMitre <nicolasmelluso7@gmail.com>
2022-10-29 18:45:11 -0300

    add .env and configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { TrainerModule } from './trainer/trainer.module';
 import { GymModule } from './gym/gym.module';
 
 @Module({
-  imports: [PokemonModule, TrainerModule, GymModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PokemonModule,
+    TrainerModule,
+    GymModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
